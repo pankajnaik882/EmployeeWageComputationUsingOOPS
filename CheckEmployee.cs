bb460a6d9c4e2e292080bcd7b6130f78f4c7d276
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-17 16:49:42 +0530

    Add : Checking Employee is Present or Absent

---
+++ CheckEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationUsingOOPS
+{
+    public class CheckEmployee
+    {
+        public void checkEmployee()
+        {
+            int isFullTime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
