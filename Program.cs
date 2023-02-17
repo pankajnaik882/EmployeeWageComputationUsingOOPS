b6532265e33d2174beda2e737bf47a07e28a94ee
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-17 18:16:45 +0530

    Add : Calculating Monthly Wages for Multiple Companies

--- Program.cs
+++ Program.cs
@@ -6,8 +6,13 @@ namespace EmployeeWageComputationUsingOOPS
     {
         public static void Main(string[] args)
         {
-            CalculateMonthlyHrsWages cmhR = new CalculateMonthlyHrsWages();
-            cmhR.montlyWage();
+            EmployeeWageMultiple e1 = new EmployeeWageMultiple();
+            int a = e1.EmployeeWage("BridgeLabz", 20, 20, 100);
+            Console.WriteLine("First Company :" + a);
+
+            EmployeeWageMultiple e2 = new EmployeeWageMultiple();
+            int b = e2.EmployeeWage("PankajPvtLtd", 18, 16, 100);
+            Console.WriteLine("Second Company : " + b);
         }
     }
 }
